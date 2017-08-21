16a9b715771b30408d44788ad6f181b606bfd6e5
Brian Huang <yangleihuang@gmail.com>
2017-08-22 00:07:41 +1000

    Add "unit test"

--- test/unit/test.spec.js
+++ test/unit/test.spec.js
@@ -1,8 +1,6 @@
-
-
-describe('des', ()=> {
-  it("it", () => {
-    const value = 1;
-    value.should.eql(1);
-  });
+describe("Shouldn't integration test be better for GraphQL service test??", () => {
+  it("Or does it make sense to test schema in unit test?", () => {
+    const value = true;
+    value.should.eql(true);
+  })
 })
